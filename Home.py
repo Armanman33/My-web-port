2788e7c0183aa61da9897794c5965f0e0592e47a
Arman <bayaterfan0@gmail.com>
2025-01-26 07:36:48 +0330

    Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('D:\Gongzuo\My-webproject\images2\Hell.png', width=600)
+    st.image('https://raw.githubusercontent.com/Armanman33/My-web-port/master/images2/Hell.png', width=600)
 
 with col2:
     st.title('Introduction')
